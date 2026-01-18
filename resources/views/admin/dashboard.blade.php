3ee58f1bf97a084cf9ca22e4159f6fc536e6cb01
RatmanDev <suratman@hype-plus.com>
2026-01-19 00:58:02 +0700

    updte

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <h2>Admin Dashboard</h2>
+    <p>Selamat datang</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix(env('ADMIN_DIR')) ->group(
+    function() {
+        Route::controller(\App\Http\Controllers\Admin\DashboardController::class)->group(
+            function () {
+                Route::get('/dashboard', 'index')->name('admin.dashboard');
+            }
+        );
+
+
+        //Posts
+        Route::prefix('posts') ->group(
+            function() {
+                Route::controller(\App\Http\Controllers\Admin\PostController::class)->group(
+                    function () {
+                        Route::get('/', 'index')->name('admin.posts.index');
+                        Route::get('/create', 'create')->name('admin.posts.create');
+                        Route::post('/store', 'store')->name('admin.posts.store');
+                        Route::get('/edit/{id}', 'edit')->name('admin.posts.edit');
+                        Route::post('/update/{id}', 'update')->name('admin.posts.update');
+                        Route::post('/delete', 'delete')->name('admin.posts.delete');
+                    }
+                );
+            }
+        );
+    }
+);
+
+
+
