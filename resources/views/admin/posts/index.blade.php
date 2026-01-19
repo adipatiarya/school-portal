0b259f354e18c4fe977008bb4ddff50047bafc4b
RatmanDev <suratman@hype-plus.com>
2026-01-19 14:31:51 +0700

    updte

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,7 +9,73 @@
 @section('content')
   <div class="container-fluid mb-5">
       <section>
-          <h2 class="font-weight-bold mt-lg-5 mb-5 pb-4"><strong>Post list</strong></h2>
+        
+           <!--Top Table UI-->
+          <div class="card p-2 mb-5">
+
+              <!--Grid row-->
+              <div class="row">
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-12">
+
+                      <!--Name-->
+                      <select class="mdb-select colorful-select dropdown-primary mx-2 md-form mt-3">
+                      <option value="" disabled selected>Bulk actions</option>
+                      <option value="1">Delete</option>
+                      <option value="2">Export</option>
+                  </select>
+
+                  </div>
+                  <!--Grid column-->
+
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6">
+
+                      <!--Blue select-->
+                      <select class="mdb-select colorful-select dropdown-primary mx-2 md-form mt-3" multiple>
+                        <option value="" disabled selected>Filter by Categories</option>
+                        <option value="2">Never opened <span> (200)</span></option>
+                        <option value="3">Opened but unanswered <span> (1800)</span></option>
+                        <option value="4">Answered <span> (200)</span></option>
+                        <option value="5">Unsunscribed <span> (50)</span></option>
+                      </select>
+                      <!--/Blue select-->
+
+                  </div>
+                  <!--Grid column-->
+
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6">
+
+                      <!--Blue select-->
+                      <select class="mdb-select colorful-select dropdown-primary mx-2 md-form mt-3" multiple>
+                        <option value="" disabled selected>Filter by Tags</option>
+                        <option value="1">Contacts in no segments <span> (100)</span></option>
+                        <option value="1">Segment 1 <span> (2000)</span></option>
+                        <option value="2">Segment 2 <span> (1000)</span></option>
+                        <option value="3">Segment 3 <span> (4000)</span></option>
+                      </select>
+                      <!--/Blue select-->
+
+                  </div>
+                  <!--Grid column-->
+
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6">
+
+                      <form class="form-inline md-form mt-2 ml-2">
+                          <input class="form-control mt-2" type="text" placeholder="Search" style="max-width: 150px;">
+                          <button class="btn btn-sm btn-primary ml-2 px-1"><i class="fa fa-search"></i>  </button>
+                      </form>
+
+                  </div>
+                  <!--Grid column-->
+
+              </div>
+              <!--Grid row-->
+
+          </div>
+                <!--Top Table UI-->
           <div class="row">
             
               <div class="col-md-12">                     
@@ -64,6 +130,7 @@
         var table= $('#datatables').DataTable({
           processing: true,       // tampilkan indikator loading
           serverSide: true,       // aktifkan server-side mode
+          searching: false,
           ajax: {
               url: "{{ route('admin.posts.index') }}",  // endpoint API Laravel
               type: 'GET',         // atau 'POST' sesuai kebutuhan
@@ -92,6 +159,7 @@
     // Material Select Initialization
     $(document).ready(function () {
         $('select[name="datatables_length"]').material_select();
+        $('.mdb-select').material_select();
     });
 </script>
 @endsection
